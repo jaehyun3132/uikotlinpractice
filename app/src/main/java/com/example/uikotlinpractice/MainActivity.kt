e1d6e497f31d1d775b5cc0b6b1e55b42abc337fb
jaehyun <wogus3132@naver.com>
2021-09-29 11:15:51 +0900

    입력한 문구를 반옇해보기

--- app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
@@ -9,6 +9,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        contentEdt
+
+//        확인버튼이눌리면 이벤트를 달아주자.
+        okBtn. setOnClickListener {
+//            입련된 내용? ; contentEdt 의 text 의 값 조회
+        //            조회(get)변수에 저장해두자
+//            저장해둔 입력 값을 텍스트뷰에 반영(set)
+
+            val inputContent =  contentEdt.text.toString()
+
+            resultTxt.text = inputContent
+
+
+
+
+       }
     }
 }
